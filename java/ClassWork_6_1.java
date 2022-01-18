f92e10d2b15f9ab95dd92f6c623f330c05301962
pookmaster21 <landa.lavi@gmail.com>
2022-01-18 19:17:48 +0200

    Added New Game and Top Ten

---
+++ java/ClassWork_6_1.java
@@ -0,0 +1,204 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class ClassWork_6_1 {
+    public static Scanner reader = new Scanner(System.in);
+    public static void Ex1() {
+        for(int i=1;i<=10;i++) {
+            System.out.printf("Enter the id for person number %s:", i);
+            String id = reader.next();
+            if (id.charAt(0) == '5') {
+                System.out.println("44" + id.substring(1));
+            } else {
+                System.out.println(id);
+            }
+        }
+    }
+    public static void Ex2() {
+        int count = 0;
+        for(int i=1;i<=10;i++) {
+            System.out.printf("Enter the first word of pair number %s:", i);
+            String word1 = reader.next();
+            System.out.printf("Enter the second word of pair number %s:", i);
+            String word2 = reader.next();
+            if (word1.length() == word2.length() && word1.charAt(0) == word2.charAt(0) && word1.charAt(word1.length()-1) == word2.charAt(word2.length()-1)) {
+                count++;
+            }
+        }
+        System.out.println("There are " + count + " similar words");
+    }
+    public static String Ex3(String word) {
+        String new_word = "";
+        for (int i=0;i<word.length();i++) {
+            new_word = word.charAt(i) + new_word;
+        }
+        return new_word;
+    }
+    public static String Ex4(String word) {
+        String new_word = "";
+        for (int i=0;i<word.length();i++) {
+            new_word = new_word + word.charAt(i) + word.charAt(i);
+        }
+        return new_word;
+    }
+    public static String Ex5(String word) {
+        String new_word = "";
+        for (int i=0;i<word.length();i++) {
+            if (word.charAt(i) != ' ') {
+                new_word = new_word + word.charAt(i);
+            }
+        }
+        return new_word;
+    }
+    public static int Ex6(String word, char letter) {
+        int count = 0;
+        for (int i=0; i<=word.length()-1;i++) {
+            if (word.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static String Ex7(String word) {
+        String new_word = "";
+        for (int i = 0; i < word.length(); i++) 
+        {
+            if (new_word.indexOf(word.charAt(i)) < 0)
+            {
+                new_word += word.charAt(i);
+            }
+        }
+        return new_word;
+    }
+    public static String Ex8(String word) {
+        String new_word = "";
+        for (int i=0;i<word.length();i++) {
+            if ((word.charAt(i) >= 65 && word.charAt(i) <= 90) || (word.charAt(i) >= 97 && word.charAt(i) <= 122)) {
+                new_word += i+1;
+            } else {
+                new_word += word.charAt(i);
+            }
+        }
+        return new_word;
+    }
+    public static void Ex9(String word1, String word2) {
+        int count = 0;
+        for (int i=0;i<word1.length()-word2.length();i++) {
+            if (word1.substring(i, i+word2.length()).equals(word2)) {
+                count++;
+            }
+        }
+    }
+    public static String Ex10(String word1, String word2) {
+        String new_word = "";
+        for (int i = 0; i < word1.length(); i++) 
+        {
+            if (new_word.indexOf(word1.charAt(i)) < 0 && word2.indexOf(word1.charAt(i)) >= 0)
+            {
+                new_word += word1.charAt(i);
+            }
+        }
+        return new_word;
+    }
+    public static String Ex11(String word1, String word2) {
+        String new_word = "";
+        for (int i = 0; i < word1.length(); i++) 
+        {
+            if (new_word.indexOf(word1.charAt(i)) < 0)
+            {
+                new_word += word1.charAt(i);
+            }
+        }
+        for (int i = 0; i < word2.length(); i++) 
+        {
+            if (new_word.indexOf(word2.charAt(i)) < 0)
+            {
+                new_word += word2.charAt(i);
+            }
+        }
+        return new_word;
+    }
+    public static void Ex12() {
+        String[] words = new String[20];
+        for (int i=0;i<20;i++) {
+            System.out.printf("Enter string number %s:", i+1);
+            words[i] = reader.next();
+        }
+        for(int i=0;i<20;i++) {
+            for(int g=0;g<20;g++) {
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String word, word2;
+        System.out.print("Which exercise do you want to test: ");
+        int ex = reader.nextInt();
+        String blank = reader.nextLine();
+        while (true) {
+            switch (ex) {
+                case 1:
+                    Ex1();
+                    break;
+                case 2:
+                    Ex2();
+                    break;
+                case 3:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println(Ex3(word));
+                    break;
+                case 4:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println(Ex4(word));
+                    break;
+                case 5:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println(Ex5(word));
+                    break;
+                case 6:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println("Enter a letter");
+                    char letter = reader.next().charAt(0);
+                    System.out.println(Ex6(word, letter));
+                    break;
+                case 7:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println(Ex7(word));
+                    break;
+                case 8:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println(Ex8(word));
+                    break;
+                case 10:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println("Enter another string");
+                    word2 = reader.nextLine();
+                    System.out.println(Ex10(word, word2));
+                    break;
+                case 11:
+                    System.out.println("Enter a string");
+                    word = reader.nextLine();
+                    System.out.println("Enter another string");
+                    word2 = reader.nextLine();
+                    System.out.println(Ex11(word, word2));
+                    break;
+                case 12:
+                    Ex12();
+                    break;
+                default:
+                    System.out.print("Invalid input try again");
+                    break;
+            }
+            System.out.print("Which exercise do you want to check: ");
+            ex = reader.nextInt();
+            blank = reader.nextLine();
+        }
+    }
+}
--- py/TicTacToeWithGraphices.py
+++ py/TicTacToeWithGraphices.py
@@ -3,28 +3,38 @@ author: Lavi Jacob Landa
 date: 12/15/2021
 explain: The program is a tic tac toe game!
 """
+from hashlib import new
 from tkinter import *
 from tkinter import messagebox
 
 
-# disable all the buttons
+# Disable all the buttons
 def disable_all_buttons():
     for i in range(3):
         for g in range(3):
             b_list[i][g].config(state=DISABLED)
 
 
-# swapping players
-def swap_players():
-    global player
-    player = "X" if player == "O" else "O"
+def read_win_file():
+    try:
+        with open(r"TicTacToe.txt", "r") as file:
+            lines = []
+            count = 1
+            while(line := file.readline().rstrip()):
+                lines.append(line)
+                if count == 10:
+                    break
+                count =+ 1
+            return lines
+    except FileNotFoundError:
+        open(r"TicTacToe.txt", "w")
 
 
 # Check to see if someone won
 def checkifwon(player):
     winner = False
 
-    # checking rows and colums
+    # Checking rows and colums
     for i in range(3):
         if b_list[i][0]["text"] == player and\
                 b_list[i][1]["text"] == player and\
@@ -39,7 +49,7 @@ def checkifwon(player):
                 b_list[g][i].config(bg="red")
             winner = True
 
-    # checking diagonals
+    # Checking diagonals
     if b_list[0][0]["text"] == player and\
             b_list[1][1]["text"] == player and\
             b_list[2][2]["text"] == player:
@@ -54,13 +64,22 @@ def checkifwon(player):
         b_list[2][0].config(bg="red")
         b_list[1][1].config(bg="red")
         winner = True
+
+    # Win message
     if winner:
         messagebox.showinfo("Tic Tac Toe", f"CONGRATULATIONS! {player} Wins!!")
         disable_all_buttons()
+        old_lines = read_win_file()
+        new_lines = [player + "\n"]
+        if old_lines is not None:
+            for i in range(len(old_lines)):
+                new_lines.append(old_lines[i] + "\n")
+        with open(r"TicTacToe.txt", "w") as file:
+            file.writelines(new_lines)
 
     # Checking if it's a tie
-    tie = True
     if not(winner):
+        tie = True
         for i in range(3):
             for g in range(3):
                 if b_list[i][g]["text"] == " ":
@@ -68,6 +87,13 @@ def checkifwon(player):
         if tie:
             messagebox.showinfo("Tic Tac Toe", "It's A Tie!\n No One Wins!")
             disable_all_buttons()
+            old_lines = read_win_file()
+            new_lines = ["Tie\n"]
+            if old_lines is not None:
+                for i in range(len(old_lines)):
+                    new_lines.append(old_lines[i] + "\n")
+            with open(r"TicTacToe.txt", "w") as file:
+                file.writelines(new_lines)
 
 
 # Button clicked function
@@ -77,15 +103,61 @@ def b_click(b: Button):
     if b["text"] == " ":
         b.config(text=player)
         checkifwon(player)
-        swap_players()
+        # Swapping players
+        player = "X" if player == "O" else "O"
     else:
         messagebox.showerror("Tic Tac Toe",
                              "Hey! That box has already been selected" +
                              "\nPick Another Box...")
 
 
+# Rules window
+def rules():
+    rules_win = Toplevel()
+    rules_win.title("Rules")
+    rules_win.grab_set()
+    title = Label(rules_win, text="RULES FOR TIC-TAC-TOE.",
+                  font=("Helvetica", 20))
+    rules = Label(rules_win,
+                  text="1. The game is played on a grid that's 3 squares by " +
+                       "3 squares.\n2. You are X, your friend or the compute" +
+                       "r is O.Players take turns putting their marks in emp" +
+                       "ty squares.\n3. The first player to get 3 of his mar" +
+                       "ks in a row (up, down, across, or diagonally) is the" +
+                       " winner.\n4. When all 9 squares are full, the game i" +
+                       "s over. If no player has 3 marks in a row, the game " +
+                       "ends in a tie.",
+                  font=("Helvetica", 20))
+    title.grid(row=0, column=0)
+    rules.grid(row=1, column=0)
+
+
+# Top Ten window
+def top_ten():
+    top_ten_win = Toplevel()
+    top_ten_win.title("Top Ten")
+    top_ten_win.grab_set()
+    lines = read_win_file()
+    if lines is not None:
+        if len(lines) < 10:
+            times = len(lines)
+        else:
+            times = 10
+        for i in range(times):
+            if lines[i] == "Tie":
+                line = Label(top_ten_win, text=f"{i+1}. Tie",
+                             font=("Helvetica", 20))
+            else:
+                line = Label(top_ten_win, text=f"{i+1}. {lines[i]} won",
+                             font=("Helvetica", 20))
+            line.pack()
+    else:
+        line = Label(top_ten_win, text="No Games Found",
+                     font=("Helvetica", 20))
+
+
 # Restart the game over!
-def reset():
+def reset_game():
     global b_list, player
     player = "X"
 
@@ -113,19 +185,26 @@ def reset():
         for g in range(3):
             b_list[i][g].grid(row=i, column=g)
 
+    # Build our rules button
+    rules_btn = Button(root, text="Rules", font=("Helvetica", 20), height=3,
+                       width=6, command=rules)
+    rules_btn.grid(row=3, column=0)
+
+    # Build our new game button
+    new_game_btn = Button(root, text="New Game", font=("Helvetica", 20),
+                          height=3, width=6, command=reset_game)
+    new_game_btn.grid(row=3, column=1)
+
+    # Build our top ten button
+    top_ten_btn = Button(root, text="Top Ten", font=("Helvetica", 20),
+                         height=3, width=6, command=top_ten)
+    top_ten_btn.grid(row=3, column=2)
+
+
 if __name__ == '__main__':
     root = Tk()
-    root.title('Tic-Tac-Toe')
+    root.title('Menu')
 
-    # Create menue
-    my_menu = Menu(root)
-    root.config(menu=my_menu)
-
-    # Create Options Menu
-    options_menu = Menu(my_menu, tearoff=False)
-    my_menu.add_cascade(label="Options", menu=options_menu)
-    options_menu.add_command(label="Rest Game", command=reset)
-
-    reset()
+    reset_game()
 
     root.mainloop()
